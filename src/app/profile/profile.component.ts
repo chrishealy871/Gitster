f0619a6f075545effe6511cd5de10daed0c52e82
Chris Healy <chrishealy@Chriss-MacBook-Air.local>
2017-02-03 11:06:58 +0000

    updated indent on ts files

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,33 +1,34 @@
-import {Component, OnInit} from '@angular/core';
-import {GithubService} from '../github.service';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css']
 
 })
 
 export class ProfileComponent implements OnInit {
-	private user:any[]
-	private repos:any[]
-	private username:any;
+	private user: any[]
+	private repos: any[]
+	private username: any;
 
-  constructor(private _githubService:GithubService) {
+	constructor(private _githubService: GithubService) {
 
-}
-  	searchUser()
- {
- this._githubService.updateUser(this.username);
+	}
+	searchUser() {
+		this._githubService.updateUser(this.username);
 
-	this._githubService.getUser().subscribe(user => {
- 	this.user = user} )
+		this._githubService.getUser().subscribe(user => {
+			this.user = user
+		})
 
-	this._githubService.getRepos().subscribe(repos => {
- 	this.repos = repos} )
-  
-}
-  ngOnInit() {
-  }
+		this._githubService.getRepos().subscribe(repos => {
+			this.repos = repos
+		})
+
+	}
+	ngOnInit() {
+	}
 
 }
